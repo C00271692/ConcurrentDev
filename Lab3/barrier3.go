f7bdf0b18b29b1e34ef8e359fa8cfa6bb36b8117
Kacper_K <113976235+C00271692@users.noreply.github.com>
2024-10-14 12:07:57 +0100

    Add files via upload

---
+++ Lab3/barrier3.go
@@ -0,0 +1,82 @@
+//Barrier.go Template Code
+//Copyright (C) 2024 Kacper Krakowiak
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//--------------------------------------------
+// Author: Kacper Krakowiak (C00271692)
+// Created on 30/9/2024
+// Modified by:
+// Description:
+// A simple barrier implemented using mutex and unbuffered channel
+// Issues:
+// many
+//--------------------------------------------
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Place a barrier in this function --use Mutex's and Semaphores
+func doStuff(goNum int, arrived *int, max int, wg *sync.WaitGroup, sharedLock *sync.Mutex, theChan chan bool) bool {
+	time.Sleep(time.Second)
+	fmt.Println("Part A", goNum)
+	//we wait here until everyone has completed part A
+	sharedLock.Lock()
+	*arrived++
+	if *arrived == max { //last to arrive -signal others to go
+		sharedLock.Unlock() //unlock before any potentially blocking code
+		theChan <- true
+		<-theChan
+	} else { //not all here yet we wait until signal
+		sharedLock.Unlock() //unlock before any potentially blocking code
+		<-theChan
+		theChan <- true //once we get through send signal to next routine to continue
+	} //end of if-else
+	fmt.Println("Part B", goNum)
+	sharedLock.Lock()
+	*arrived--
+	sharedLock.Unlock()
+	if *arrived == 0 {
+		sharedLock.Unlock()
+		for range max - 1 {
+			theChan <- true
+		}
+	} else {
+		sharedLock.Unlock()
+		<-theChan
+	}
+
+	fmt.Println("PartC", goNum)
+	wg.Done()
+	return true
+} //end-doStuff
+
+func main() {
+	totalRoutines := 10
+	arrived := 0
+	var wg sync.WaitGroup
+	wg.Add(totalRoutines)
+	//we will need some of these
+	var theLock sync.Mutex
+	theChan := make(chan bool)     //use unbuffered channel in place of semaphore
+	for i := range totalRoutines { //create the go Routines here
+		go doStuff(i, &arrived, totalRoutines, &wg, &theLock, theChan)
+	}
+	wg.Wait() //wait for everyone to finish before exiting
+} //end-main
